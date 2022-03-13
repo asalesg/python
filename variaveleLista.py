c8461efb75eda3841b2da6c329d2757ea6635568
André Sales <asalesg@gmail.com>
2022-03-13 15:04:17 -0300

    Create variaveleLista.py

---
+++ variaveleLista.py
@@ -0,0 +1,13 @@
+inventario=[]
+resposta="S"
+while resposta=="S":
+    inventario.append(input("Equipamento: "))
+    inventario.append(float(input("Valor: ")))
+    inventario.append(int(input("Número Serial: ")))
+    inventario.append(input("Departamento: "))
+    resposta=input("Digite \"S\"para continuar: ").upper()
+
+for elemento in inventario:
+    print(elemento)
+
+    #função do append é inserir dado dentro da lista
