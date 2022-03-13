0fc45c81d713a2f8d9f7d615c0c046dc06c4b7fe
André Sales <asalesg@gmail.com>
2022-03-13 13:00:04 -0300

    Initial commit

---
+++ fisica.py
@@ -0,0 +1,8 @@
+#esse programa calcula a velocidade media
+
+print ("Esse é o calculador de velocidade media")
+distancia = input("Digite a distancia percorrida: ")
+tempo = input ("Digite o tempo percorrido: ")
+velocidade_media = float (distancia) / float (tempo)
+print("A velocidade media calculada foi de {:.2f} km/h" .format(velocidade_media))15
+#dentro das chaves eu formatei a resposta em colcoar 2 casas decimais no formato FLOAT
---
+++ lição_python.py
@@ -0,0 +1,4 @@
+valor1 = input("digite o primeiro valor")
+valor2 = input("digite o segundo valor")
+soma = int(valor1) + int(valor2)
+print ("A soma é {}" .format(soma)
