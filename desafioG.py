b596f5ea418730f74538c8654ad4b9e720d4660e
André Sales <asalesg@gmail.com>
2022-03-14 16:11:15 -0300

    Update desafioG.py

--- desafioG.py
+++ desafioG.py
@@ -1,3 +1,3 @@
 n1=float(input('Insira a nota da primeira avaliação: '))
 n2=float(input('Insira a nota da segunda avaliação: '))
-print('A média final das avaliações é {}.' .format((n1+n2)/2))
+print('A média final das avaliações é {:.1f}.' .format((n1+n2)/2))
