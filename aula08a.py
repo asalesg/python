818789d9f88a738f5b9cdb0ff61df0804b25c593
Andre Sales <asalesg@gmail.com>
2022-03-16 16:49:52 -0300

    Create aula08a.py

---
+++ aula08a.py
@@ -0,0 +1,14 @@
+import math
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print("A raiz de {} é igual a {:.2f}. " .format(num, raiz))
+
+# ou tudo isso //from math import sqrt
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print("A raiz de {} é igual a {:.2f}. " .format(num, raiz))
+
+import emoji
+
+print(emoji.emojize("Olá mundo :red_heart:" use_aliases=true))
