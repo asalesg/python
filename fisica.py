b36fd1e14de8415c7124d27a7a55e7b9313c3778
Andre Sales <asalesg@gmail.com>
2022-03-20 19:44:05 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -4,5 +4,5 @@ print ("Esse é o calculador de velocidade media")
 distancia = input("Digite a distancia percorrida: ")
 tempo = input ("Digite o tempo percorrido: ")
 velocidade_media = float (distancia) / float (tempo)
-print("A velocidade media calculada foi de {:.2f} km/h" .format(velocidade_media))15
+print("A velocidade media calculada foi de {:.2f} km/h" .format(velocidade_media))
 #dentro das chaves eu formatei a resposta em colcoar 2 casas decimais no formato FLOAT
