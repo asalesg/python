0030152c8b442284d1f2cf73326253eda8bd0c8d
Andre Sales <asalesg@gmail.com>
2022-03-26 11:32:58 -0300

    Update cap2ex8.py

--- cap2ex8.py
+++ cap2ex8.py
@@ -1,26 +1,26 @@
 print("Verificador de frequências cardíacas")
-idade = int(input("Por favor, informe sua idade"))
-bpm = int(input("Por favor, informe seus batimentos"))
+idade = int(input("Por favor, informe sua idade: "))
+bpm = int(input("Por favor, informe seus batimentos: "))
 
 if idade <= 2:
     if bpm >= 120 and bpm <= 140:
-        print("Frequencia cardiaca adequada")
+        print("Frequencia cardiaca adequada ")
     else: 
-        print("Freqiencia cardiaca inadequada")
+        print("Freqiencia cardiaca inadequada ")
 elif idade >=8 and idade < 17:
     if bpm >= 80 and bpm <= 100:
-        print("Frequencia cardiaca adequada")
+        print("Frequencia cardiaca adequada ")
     else: 
-        print("Freqiencia cardiaca inadequada")
+        print("Freqiencia cardiaca inadequada ")
 elif idade >=18 and idade < 60:
     if bpm >= 70 and bpm <= 80:
-        print("Frequencia cardiaca adequada")
+        print("Frequencia cardiaca adequada ")
     else: 
-        print("Freqiencia cardiaca inadequada")
+        print("Freqiencia cardiaca inadequada ")
 elif idade >=60:
     if bpm >= 50 and bpm <= 60:
-        print("Frequencia cardiaca adequada")
+        print("Frequencia cardiaca adequada ")
     else: 
-        print("Freqiencia cardiaca inadequada")
+        print("Freqiencia cardiaca inadequada ")
 else:
-    print("Não existem dados para a idaed indicada")
+    print("Não existem dados para a idade indicada ")
