5b0b87c759871037983b705c6f1377aaf1fe82b1
Andre Sales <asalesg@gmail.com>
2022-03-30 13:39:43 -0300

    Create ex1loop.py

---
+++ ex1loop.py
@@ -0,0 +1,7 @@
+quantidade_alimentos = int(input("Por favor, informe quantos alimentos voce consumiu hoje "))
+total_calorias = 0
+for alimento in range (1, quantidade_alimentos + 1, 1):
+caloria = int(input("Informe a quantidade de calorias do {} do alimento " .format(alimento)))
+total_calorias = total_calorias + caloria
+
+print("Foram consumidas {} calorias ao longo do dia" .format(total_calorias))
