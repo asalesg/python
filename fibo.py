60eed5352ba88c06cacb26b347c74abf9803e697
Andre Sales <asalesg@gmail.com>
2022-03-30 16:07:03 -0300

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,14 @@
+numero_usuario = int(input("Por favor, informe um numero inteiro: "))
+anterior1 = 1
+anteror2 = 0
+for n_elemento in range(1, numero_usuario + 1, 1):
+    atual = anterior1 + anteror2
+    anterior1 = anteror2
+    anteror2 = atual
+    if numero_usuario == atual:
+        print("Ação bem sucedida")
+        break
+    elif numero_usuario < atual:
+        print("Ação falhou")
+        break
+    
