ca83ee66463cde5edd0662110dc602817489e04c
Andre Sales <asalesg@gmail.com>
2022-03-30 19:36:43 -0300

    att

--- felipimMulheki.py
+++ felipimMulheki.py
@@ -2,8 +2,6 @@ valor_bruto = float(input("Por favor, informe o valor bruto da viagem: "))
 categoria = input("Por favor, informe a categoria: ECONÔMICA, EXECUTIVA ou PRIMEIRA CLASSE: ")
 quantidade_viajante = int(input("Por favor, informe a quantidade de viajantes: "))
 valor_desconto = 0
-
-
 if categoria.upper() == "ECONÔMICA":
     if quantidade_viajante == 2:
         valor_desconto = valor_bruto * 0.03
@@ -33,8 +31,4 @@ elif categoria.upper() == "PRIMEIRA CLASSE":
         print("Olha eu perdido de novo aqui. Sou um código que não tem sei o que fazer nesta condição de else")
 else:
     print("Categoria inexistente, não será concedido nenhum desconto")
-
-    valor_liquido == (valor_bruto - valor_desconto)
-    media_viajante == (valor_liquido / quantidade_viajante)
-
-    print ("O Valor da viagem é de R${}. Apos os descontos de R${} a viagem custara R${}. Cada passageiro tem um custo medio de R${}" .format(valor_bruto, valor_desconto, valor_liquido, media_viajante))
+    valor_liquido = valor_bruto - valor_desconto
