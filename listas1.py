80c175f7ddda751321c6fba2c80ecaecc6403c91
Andre Sales <asalesg@gmail.com>
2022-04-08 17:04:54 -0300

    att

---
+++ listas1.py
@@ -0,0 +1,15 @@
+lista = ["guitarra", "baixo", "violao"]
+
+#lista.append(input("digite o novo instrumento: ")) insere no final
+
+#lista.insert(2, "violino")insere na posição que quero
+
+#lista.pop() tira o ultimo da lista
+
+#lista.remove("nome do instrumento") tiro o elemento
+
+#print(len(lista)) tamanho da lista qtos itens
+
+#print(lista.count("o que busco na lista")) mostra qtas tem na lista
+
+#lista.sort() fica em ordem alfabetica lista.reverse() deixa ao contrario
