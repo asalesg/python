4d3bce1b88c17acbe11679f45c0c37eff14cfe0f
Andre Sales <asalesg@gmail.com>
2022-04-11 10:00:48 -0300

    att

--- rm94134_ex03.py
+++ rm94134_ex03.py
@@ -1,21 +1,19 @@
 total_alunos = 50
-media_par = 0
-media_impar = 0
+soma_impar = 0
+soma_par = 0
 for impar in range (1, total_alunos, 2):
     print("VOCÊ ESTÁ DIGITANDO AS NOTAS DOS ALUNOS IMPARES" )
-    soma_impar = int(input("POR FAVOR, INSIRA A NOTA DO ALUNO DE NÚMERO {}: "  .format(impar)))
+    soma_impar += float(input("POR FAVOR, INSIRA A NOTA DO ALUNO DE NÚMERO {}: "  .format(impar)))
+    
     
 for par in range (2, total_alunos + 2, 2):
     print("VOCÊ ESTÁ DIGITANDO AS NOTAS DOS ALUNOS PARES")
-    soma_par = int(input("POR FAVOR, INSIRA A NOTA DO ALUNO DE NÚMERO {}: "  .format(par)))
+    soma_par += float(input("POR FAVOR, INSIRA A NOTA DO ALUNO DE NÚMERO {}: "  .format(par)))
     
-media_par = media_par + soma_par / 25
-media_impar = media_impar + soma_impar / 25
+print("A média da parte par foi: {} " .format(soma_par/25))
+print("A média da parte impar foi: {} " .format(soma_impar/25))
 
-print("A média da parte par foi: {} " .format(media_par))
-print("A média da parte impar foi: {} " .format(media_impar))
-
-if media_par > media_impar:
+if soma_par > soma_impar:
     print("A parte par teve média maior.")
 else:
     print("A parte impar teve média maior.")
